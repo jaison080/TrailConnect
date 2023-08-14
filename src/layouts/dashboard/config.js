aafb4d4eebd0e40af647b40d5413c04c34fbdd52
Jaison Dennis <jaisondennis080@gmail.com>
2023-08-14 14:03:54 +0530

    UI Somewhat Complete

--- src/layouts/dashboard/config.js
+++ src/layouts/dashboard/config.js
@@ -1,76 +1,85 @@
-import ChartBarIcon from '@heroicons/react/24/solid/ChartBarIcon';
-import CogIcon from '@heroicons/react/24/solid/CogIcon';
-import LockClosedIcon from '@heroicons/react/24/solid/LockClosedIcon';
-import ShoppingBagIcon from '@heroicons/react/24/solid/ShoppingBagIcon';
-import UserIcon from '@heroicons/react/24/solid/UserIcon';
-import UserPlusIcon from '@heroicons/react/24/solid/UserPlusIcon';
-import UsersIcon from '@heroicons/react/24/solid/UsersIcon';
-import XCircleIcon from '@heroicons/react/24/solid/XCircleIcon';
-import { SvgIcon } from '@mui/material';
+import ChartBarIcon from "@heroicons/react/24/solid/ChartBarIcon";
+import CogIcon from "@heroicons/react/24/solid/CogIcon";
+import LockClosedIcon from "@heroicons/react/24/solid/LockClosedIcon";
+import ShoppingBagIcon from "@heroicons/react/24/solid/ShoppingBagIcon";
+import UserIcon from "@heroicons/react/24/solid/UserIcon";
+import UserPlusIcon from "@heroicons/react/24/solid/UserPlusIcon";
+import UsersIcon from "@heroicons/react/24/solid/UsersIcon";
+import XCircleIcon from "@heroicons/react/24/solid/XCircleIcon";
+import { SvgIcon } from "@mui/material";
 
 export const items = [
   {
-    title: 'Overview',
-    path: '/',
+    title: "Overview",
+    path: "/",
     icon: (
       <SvgIcon fontSize="small">
         <ChartBarIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Items',
-    path: '/items',
+    title: "Items",
+    path: "/items",
     icon: (
       <SvgIcon fontSize="small">
         <UsersIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Warehouses',
-    path: '/warehouses',
+    title: "Warehouses",
+    path: "/warehouses",
     icon: (
       <SvgIcon fontSize="small">
         <ShoppingBagIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Locations',
-    path: '/locations',
+    title: "Transports",
+    path: "/transports",
+    icon: (
+      <SvgIcon fontSize="small">
+        <UsersIcon />
+      </SvgIcon>
+    ),
+  },
+  {
+    title: "Locations",
+    path: "/locations",
     icon: (
       <SvgIcon fontSize="small">
         <ShoppingBagIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Transports',
-    path: '/transports',
+    title: "Drivers",
+    path: "/drivers",
     icon: (
       <SvgIcon fontSize="small">
-      <UsersIcon />
-    </SvgIcon>
-    )
+        <UsersIcon />
+      </SvgIcon>
+    ),
   },
   {
-    title: 'Account',
-    path: '/account',
+    title: "Account",
+    path: "/account",
     icon: (
       <SvgIcon fontSize="small">
         <UserIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Settings',
-    path: '/settings',
+    title: "Settings",
+    path: "/settings",
     icon: (
       <SvgIcon fontSize="small">
         <CogIcon />
       </SvgIcon>
-    )
+    ),
   },
   // {
   //   title: 'Login',
