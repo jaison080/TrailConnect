7eaba6a7b5cc553d5d35ec807e616180d2ad3184
Jaison Dennis <jaisondennis080@gmail.com>
2023-08-14 13:47:05 +0530

    UI Somewhat Complete

--- src/sections/companies/company-card.js
+++ src/sections/companies/company-card.js
@@ -1,43 +1,48 @@
-import PropTypes from 'prop-types';
-import ArrowDownOnSquareIcon from '@heroicons/react/24/solid/ArrowDownOnSquareIcon';
-import ClockIcon from '@heroicons/react/24/solid/ClockIcon';
-import { Avatar, Box, Card, CardContent, Divider, Stack, SvgIcon, Typography } from '@mui/material';
+import PropTypes from "prop-types";
+import ArrowDownOnSquareIcon from "@heroicons/react/24/solid/ArrowDownOnSquareIcon";
+import ClockIcon from "@heroicons/react/24/solid/ClockIcon";
+import {
+  Avatar,
+  Box,
+  Card,
+  CardContent,
+  Divider,
+  Stack,
+  SvgIcon,
+  Typography,
+} from "@mui/material";
+import { useRouter } from "next/router";
 
 export const CompanyCard = (props) => {
   const { company } = props;
+  const router = useRouter();
 
   return (
     <Card
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100%'
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+        cursor: "pointer",
+      }}
+      onClick={() => {
+        router.push(`/warehouses/${company.id}`);
       }}
     >
       <CardContent>
         <Box
           sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            pb: 3
+            display: "flex",
+            justifyContent: "center",
+            pb: 3,
           }}
         >
-          <Avatar
-            src={company.logo}
-            variant="square"
-          />
+          <Avatar src={company.logo} variant="square" />
         </Box>
-        <Typography
-          align="center"
-          gutterBottom
-          variant="h5"
-        >
+        <Typography align="center" gutterBottom variant="h5">
           {company.title}
         </Typography>
-        <Typography
-          align="center"
-          variant="body1"
-        >
+        <Typography align="center" variant="body1">
           {company.description}
         </Typography>
       </CardContent>
@@ -50,41 +55,19 @@ export const CompanyCard = (props) => {
         spacing={2}
         sx={{ p: 2 }}
       >
-        <Stack
-          alignItems="center"
-          direction="row"
-          spacing={1}
-        >
-          <SvgIcon
-            color="action"
-            fontSize="small"
-          >
+        <Stack alignItems="center" direction="row" spacing={1}>
+          <SvgIcon color="action" fontSize="small">
             <ClockIcon />
           </SvgIcon>
-          <Typography
-            color="text.secondary"
-            display="inline"
-            variant="body2"
-          >
+          <Typography color="text.secondary" display="inline" variant="body2">
             Updated 2hr ago
           </Typography>
         </Stack>
-        <Stack
-          alignItems="center"
-          direction="row"
-          spacing={1}
-        >
-          <SvgIcon
-            color="action"
-            fontSize="small"
-          >
+        <Stack alignItems="center" direction="row" spacing={1}>
+          <SvgIcon color="action" fontSize="small">
             <ArrowDownOnSquareIcon />
           </SvgIcon>
-          <Typography
-            color="text.secondary"
-            display="inline"
-            variant="body2"
-          >
+          <Typography color="text.secondary" display="inline" variant="body2">
             {company.downloads} Downloads
           </Typography>
         </Stack>
@@ -94,5 +77,5 @@ export const CompanyCard = (props) => {
 };
 
 CompanyCard.propTypes = {
-  company: PropTypes.object.isRequired
+  company: PropTypes.object.isRequired,
 };
